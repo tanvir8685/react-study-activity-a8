b4c6608d33a5632d6264e5dd7a0c3396e7fe1c8a
tanvir <tanvirhridoy1011@gmail.com>
2022-09-29 01:26:33 +0600

    distructeing the data

--- src/components/Activity/Activity.js
+++ src/components/Activity/Activity.js
@@ -1,9 +1,10 @@
 import React from 'react';
 
 const Activity = ({activity}) => {
+    const{name,id,time,img}=activity;
     return (
         <div>
-            <h2>This is activity{activity.name}</h2>
+            <h2>This is activity  {name}</h2>
         </div>
     );
 };
