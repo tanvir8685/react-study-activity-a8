55951cef5dfaf3824a412902b567a90a1baa59d8
tanvir <tanvirhridoy1011@gmail.com>
2022-09-29 17:14:51 +0600

    break time added

--- src/components/Information/Information.js
+++ src/components/Information/Information.js
@@ -1,13 +1,26 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './Information.css'
 
 const Information = ({information}) => {
-    console.log(information)
+    const[brk,setBrk]=useState([]);
+    const[br,setbr]=useState([])
+    useEffect(()=>{
+        fetch('breakTime.json')
+        .then(res=>res.json())
+        .then(data=>setBrk(data))
+    },[brk])
+    
+    const showtime=(br)=>{
+        setbr(br)
+        
+    }
+    
     
     let total=0;
     for(const info of information){
         total=total+info.time;
     }
+   
     return (
         <div className='information'>
             <h2>Profile</h2>
@@ -18,10 +31,14 @@ const Information = ({information}) => {
             <p>SSC:2014</p>
             <p>Diploma Engineering:2018</p>
             <p>Bsc:2022</p><br /><br />
-            <h3>Add a bread------------</h3><br /><br />
+            <h3>Add a break------------</h3><br /><br />
+            {
+                brk.map(br=><button key={br.id} onClick={()=>showtime(br)} className='brk-btn'>{br.time} min</button>)
+            }
+            
             <h2>Subject Details</h2>
             <h3>Study Time:{total} min</h3>
-            <h3>Break time:</h3>
+            <h3>Break time:{br.time} min</h3>
 
 
 
